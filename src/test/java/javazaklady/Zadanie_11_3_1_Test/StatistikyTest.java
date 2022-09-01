0d96a773c29e324979e12184d2d02584a5c897eb
bernardkaminskibk <bernardkaminski.bk@gmail.com>
2022-09-01 17:02:52 +0200

    Added new tests

--- src/test/java/javazaklady/Zadanie_11_3_1_Test/StatistikyTest.java
+++ src/test/java/javazaklady/Zadanie_11_3_1_Test/StatistikyTest.java
@@ -22,9 +22,9 @@ class StatistikyTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"30,src/test/java/javazaklady/Zadanie_11_3_1_Test/text.txt",
-            "151,src/test/java/javazaklady/Zadanie_11_3_1_Test/text2.txt",
-            "605,src/test/java/javazaklady/Zadanie_11_3_1_Test/text3.txt",})
+    @CsvSource(value = {"30,TestFiles/text.txt",
+            "151,TestFiles/text2.txt",
+            "605,TestFiles/text3.txt",})
     void getPocetZnakovTest(int expectedResult, String filePath) {
         try {
             fileReader = new FileReader(filePath);
@@ -40,9 +40,9 @@ class StatistikyTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"7,src/test/java/javazaklady/Zadanie_11_3_1_Test/text.txt",
-            "25,src/test/java/javazaklady/Zadanie_11_3_1_Test/text2.txt",
-            "106,src/test/java/javazaklady/Zadanie_11_3_1_Test/text3.txt"})
+    @CsvSource(value = {"7,TestFiles/text.txt",
+            "25,TestFiles/text2.txt",
+            "106,TestFiles/text3.txt"})
     void getPocetSlovTest(int expectedResult, String filePath) {
         try {
             fileReader = new FileReader(filePath);
@@ -58,9 +58,9 @@ class StatistikyTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"5,src/test/java/javazaklady/Zadanie_11_3_1_Test/text.txt",
-            "2,src/test/java/javazaklady/Zadanie_11_3_1_Test/text2.txt",
-            "9,src/test/java/javazaklady/Zadanie_11_3_1_Test/text3.txt"})
+    @CsvSource(value = {"5,TestFiles/text.txt",
+            "2,TestFiles/text2.txt",
+            "9,TestFiles/text3.txt"})
     void getPocetRiadkovTest(int expectedResult, String filePath) {
         try {
             fileReader = new FileReader(filePath);
