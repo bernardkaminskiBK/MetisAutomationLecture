8bb766ea4423f1587b14213e48792b3c4c3859f7
bernardkaminskBK <bernardkaminski.bk@gmail.com>
2022-09-01 20:00:40 +0200

    added new test

--- src/main/java/javazaklady/Zadanie_11_3_4/SpajacSuborov.java
+++ src/main/java/javazaklady/Zadanie_11_3_4/SpajacSuborov.java
@@ -19,31 +19,15 @@ public class SpajacSuborov {
             return;
         }
 
-        String nazovZaloznySubor = args[args.length - 1];
-        spojObsahSuborov(args, nazovZaloznySubor);
-
+        //TODO: doplnit...
     }
 
     public static void spojObsahSuborov(String[] args, String nazovZaloznySubor) {
-        try {
-            PrintWriter zapisovac = new PrintWriter(nazovZaloznySubor);
 
-            for (String nazovVstupnySubor : args) {
-                Scanner citacSuboru = new Scanner(new FileReader(nazovVstupnySubor));
-
-                while (citacSuboru.hasNextLine()) {
-                    String riadok = citacSuboru.nextLine();
-                    zapisovac.println(riadok);
-                }
-
-                zapisovac.println();
-                citacSuboru.close();
-            }
-            zapisovac.close();
-
-            System.out.println("Kopirovanie prebehlo uspesne...");
-        } catch (FileNotFoundException ignored) {
-
+        for (String nazovVstupnySubor : args) {
+            //TODO: doplnit...
         }
+
     }
+
 }
--- src/test/java/javazaklady/Zadanie_11_3_4_Test/SpajacSuborovTest.java
+++ src/test/java/javazaklady/Zadanie_11_3_4_Test/SpajacSuborovTest.java
@@ -1,7 +1,5 @@
 package javazaklady.Zadanie_11_3_4_Test;
 
-
-import javazaklady.Zadanie_11_3_3.KopirovacSuborov;
 import javazaklady.Zadanie_11_3_4.SpajacSuborov;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
