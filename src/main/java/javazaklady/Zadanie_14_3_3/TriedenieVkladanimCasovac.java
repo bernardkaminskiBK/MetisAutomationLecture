2cbf0f8e8f6b42f0e2bfe51c054e2e4e6a55ccb5
bernardkaminskibk <bernardkaminski.bk@gmail.com>
2022-09-15 14:26:50 +0200

    added new test + some refactoring

--- src/main/java/javazaklady/Zadanie_14_3_3/TriedenieVkladanimCasovac.java
+++ src/main/java/javazaklady/Zadanie_14_3_3/TriedenieVkladanimCasovac.java
@@ -1,25 +1,26 @@
 package javazaklady.Zadanie_14_3_3;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class TriedenieVkladanimCasovac {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner vstup = new Scanner(System.in);
         System.out.print("Zadaj velkost pola: ");
         int velkost = vstup.nextInt();
         System.out.print("Zadaj hladane cislo: ");
         int hladaneCislo = vstup.nextInt();
 
-        // vytvorime pole a naplnime ho nahodne vygenerovanym cislami
-
         int[] pole = PoleUtil.vygenerujPoleCisel(velkost, 100);
+        System.out.println(statistiky(pole, hladaneCislo));
+    }
+
+    public static String statistiky(int[] pole, int hladaneCislo) {
+
+        String result = "";
+
+        // vytvorime pole a naplnime ho nahodne vygenerovanym cislami
         TriedicVkladanim triedic = new TriedicVkladanim(pole);
         BinarnyVyhladavac vyhladavac = new BinarnyVyhladavac(pole);
-
-        System.out.println(Arrays.toString(pole));
 
         // pouzite triedu Stopky na meranie vykonnosti algoritmu
         Stopky stopky = new Stopky();
@@ -28,23 +29,24 @@ public class TriedenieVkladanimCasovac {
         triedic.zotried();
         stopky.zastav();
 
+        result += "Uplynuly cas triedenia: " + stopky.getUplynulyCas() + " milisekund\n";
 
-        System.out.println("Uplynuly cas triedenia: " + stopky.getUplynulyCas() + " milisekund");
         stopky.vynuluj();
 
         stopky.spust();
         int pozicia = vyhladavac.vyhladaj(hladaneCislo);
 
         if (pozicia >= 0) {
+            result += "hladany prvok " + hladaneCislo + " sa naslo na pozicii " + pozicia + "\n";
+            stopky.zastav();
+            result += "Uplynuly cas vyhladavania: " + stopky.getUplynulyCas() + " milisekund";
+        } else {
+            result += "hladany prvok" + hladaneCislo + " sa v poli nenachadza\n";
+            stopky.zastav();
+            result += "Uplynuly cas vyhladvania: " + stopky.getUplynulyCas() + " milisekund";
+        }
 
-            System.out.println("hladany prvok " + hladaneCislo + " sa naslo na pozicii " + pozicia);
-            stopky.zastav();
-            System.out.println("Uplynuly cas vyhladavania: " + stopky.getUplynulyCas() + " milisekund");
-        }
-        else {
-            System.out.println("hladany prvok" + hladaneCislo + " sa v poli nenachadza");
-            stopky.zastav();
-            System.out.println("Uplynuly cas vyhladvania: " + stopky.getUplynulyCas() + " milisekund");
-        }
+        return result;
     }
+
 }
--- src/test/java/javazaklady/Zadanie_13_3_3_Test/TriedenieVyberomCasovacTest.java
+++ src/test/java/javazaklady/Zadanie_13_3_3_Test/TriedenieVyberomCasovacTest.java
@@ -3,34 +3,43 @@ package javazaklady.Zadanie_13_3_3_Test;
 import javazaklady.Zadanie_13_3_3.TriedenieVyberomCasovac;
 import org.apache.commons.lang3.Range;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class TriedenieVyberomCasovacTest {
 
     // Rozsah zbehnutie algoritmu je medzi 10 a 2000 milisekund, nie je dolezite presna hodnota trvanie zotriedenia pola ale musime urcit
     // vyssi rozsah z dovodu ze kazdemu zelezu to trva iny cas zotriedit urcity pocet dat
-    private final Range<Integer> milisecondRange = Range.between(10, 2000);
-    private final Range<Integer> nRange = Range.between(10000, 60000);
+    private final Range<Integer> milisecondsRange = Range.between(0, 2000);
+    private final Range<Integer> nRangeNumberOfElements = Range.between(1000, 60000);
+    private int actualRunningTime;
+    private int actualN;
 
-    @RepeatedTest(3)
-    void vygenerujCasBehuProgramuTest() {
-        for (int i = 1; i <= 6; i++) {
-            String generator = findIntInString(TriedenieVyberomCasovac.vygenerujCasBehuProgramu(10000, 60000, i));
+    /**
+     * V tomto teste neriesime porovnavacky konkretnych hodnot, myslienka testu vplyva v tom ze metoda testovanej triedy
+     * musi vratit retazec o informacii o vysledku behu programu cize meranie casu triedenia pola.
+     * Ocakavany retazec musi obsahovat dane hodnoty ktore sa nasledne skumaju a mi hodnotime iba to ci sa tieto hodnoty zmestia
+     * do nejakeho rozsahu (vopred s nami urcenych cisel) a ak sa zmestia tak vieme prehlasit kludne ze nazvy tried su spravne zadane a
+     * tie objekty ktore je nutne prevolat na testovanie su spravne implementovane a su k dispozicii a robia to co maju robit.
+     */
+    @ParameterizedTest()
+    @CsvSource(value = {"10000, 60000, 6", "1000, 3000, 3", "1000, 10000, 9"})
+    void vygenerujCasBehuProgramuTest(int dolnaVelkost, int hornaVelkost, int pocetBehov) {
+        for (int i = 1; i <= pocetBehov; i++) {
+            String generator = findIntInString(TriedenieVyberomCasovac.vygenerujCasBehuProgramu(dolnaVelkost, hornaVelkost, i));
+            getDataFromInputString(generator);
 
-            System.out.println(getActualNFrom(generator) + " " + getActualRunningTimeFrom(generator));
+            Assertions.assertTrue(nRangeNumberOfElements.contains(actualN) && milisecondsRange.contains(actualRunningTime));
 
-            Assertions.assertTrue(nRange.contains(getActualNFrom(generator)) && milisecondRange.contains(getActualRunningTimeFrom(generator)));
+            System.out.println(actualN + " " + actualRunningTime);
         }
     }
 
-    private static Integer getActualNFrom(String generator) {
-        return Integer.parseInt(generator.substring(0, 6).trim()); // Ziskame N cislo z stringu
-    }
+    private void getDataFromInputString(String generator) {
+        String[] numbers = generator.split(" ", 2);
 
-    private static Integer getActualRunningTimeFrom(String generator) {
-        StringBuilder builder = new StringBuilder(generator);
-        builder.replace(0, 6, "");
-        return Integer.parseInt(builder.toString()); // Ziskame milisecondy z stringu
+        this.actualN = Integer.parseInt(numbers[0]);
+        this.actualRunningTime = Integer.parseInt(numbers[1]);
     }
 
     private static String findIntInString(String str) {
