c35528ef4b7e10721a405a31bb21b657ceb10691
bernardkaminskibk <bernardkaminski.bk@gmail.com>
2022-09-17 21:38:32 +0200

    added new test cases

--- src/main/java/java_spolocna_praca_s_lektorom/webinar05/utils/StorePrintStream.java
+++ src/main/java/java_spolocna_praca_s_lektorom/webinar05/utils/StorePrintStream.java
@@ -1,12 +1,14 @@
 package java_spolocna_praca_s_lektorom.webinar05.utils;
 
 import java.io.PrintStream;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class StorePrintStream extends PrintStream {
 
     public static List<String> printList = new LinkedList<String>();
+    public static List<String> printfList = new LinkedList<String>();
 
     public StorePrintStream(PrintStream org) {
         super(org);
@@ -16,6 +18,12 @@ public class StorePrintStream extends PrintStream {
     public void println(String line) {
         printList.add(line);
         super.println(line);
+    }
+
+    @Override
+    public PrintStream printf(String format, Object... args) {
+        printfList.add(format + "," + Arrays.toString(args));
+        return super.printf(format, args);
     }
 
     public void println(int line) {
--- src/test/java/java_spolocna_praca_s_lektorom/webinar05/Zadanie_5_2_5/BankaTest.java
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar05/Zadanie_5_2_5/BankaTest.java
@@ -1,7 +1,6 @@
 package java_spolocna_praca_s_lektorom.webinar05.Zadanie_5_2_5;
 
 import java_spolocna_praca_s_lektorom.webinar05.utils.StorePrintStream;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,7 +12,8 @@ class BankaTest {
         System.setOut(new StorePrintStream(System.out));
         Banka.main(new String[0]);
 
-        Assertions.assertTrue(StorePrintStream.printList.get(0).contains("100"));
+        assertTrue(StorePrintStream.printList.get(0).contains("100"));
+        assertEquals(100, StorePrintStream.printfList.size());
     }
 
 }
