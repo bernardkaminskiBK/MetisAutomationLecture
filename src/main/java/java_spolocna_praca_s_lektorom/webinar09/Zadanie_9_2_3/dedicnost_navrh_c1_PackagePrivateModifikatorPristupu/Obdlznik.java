32e127566a152d79d646bb8b41c300fe961a4804
MartinMesko <alegremato@gmail.com>
2022-09-20 15:14:34 +0200

    repaire package

--- src/main/java/java_spolocna_praca_s_lektorom/webinar09/Zadanie_9_2_3/dedicnost_navrh_c1_PackagePrivateModifikatorPristupu/Obdlznik.java
+++ src/main/java/java_spolocna_praca_s_lektorom/webinar09/Zadanie_9_2_3/dedicnost_navrh_c1_PackagePrivateModifikatorPristupu/Obdlznik.java
@@ -1,4 +1,4 @@
-package dedicnost_navrh_c1_PackagePrivateModifikatorPristupu;
+package java_spolocna_praca_s_lektorom.webinar09.Zadanie_9_2_3.dedicnost_navrh_c1_PackagePrivateModifikatorPristupu;
 
 public class Obdlznik extends DvojrozmernyGrafickyUtvar
 {
