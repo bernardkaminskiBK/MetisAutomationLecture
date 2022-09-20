106d4fca9e418ecbe6635ce3810a3e4fac9fe574
MartinMesko <alegremato@gmail.com>
2022-09-20 15:09:08 +0200

    creating package Zadanie_9_2_2  together with classes

---
+++ src/main/java/java_spolocna_praca_s_lektorom/webinar09/Zadanie_9_2_2/volanie_konstruktora/Nadtrieda.java
@@ -0,0 +1,9 @@
+package java_spolocna_praca_s_lektorom.webinar09.Zadanie_9_2_2.volanie_konstruktora;
+
+public abstract class Nadtrieda
+{
+    public Nadtrieda()
+    {
+        System.out.println("... zavolal sa konstruktor: Nadtrieda().");
+    }
+}
---
+++ src/main/java/java_spolocna_praca_s_lektorom/webinar09/Zadanie_9_2_2/volanie_konstruktora/TriedyTester.java
@@ -0,0 +1,12 @@
+package java_spolocna_praca_s_lektorom.webinar09.Zadanie_9_2_2.volanie_konstruktora;
+
+public class TriedyTester
+{
+    public static void main(String[] args)
+    {
+        Podtrieda podtrieda = new Podtrieda();
+
+        // nevieme vytvorit objekt abstraktnej triedy priamo, t.j. volanim konstruktora
+        // Nadtrieda nadtrieda = new Nadtrieda();
+    }
+}
