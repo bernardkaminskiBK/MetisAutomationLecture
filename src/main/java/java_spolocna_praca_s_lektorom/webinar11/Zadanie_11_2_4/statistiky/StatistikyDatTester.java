9027f87cf5bffd0ce7d61c9468332ad98c3d0b38
MartinMesko <alegremato@gmail.com>
2022-09-21 10:22:28 +0200

    creating a package Zadanie_11_2_4 together with the class

---
+++ src/main/java/java_spolocna_praca_s_lektorom/webinar11/Zadanie_11_2_4/statistiky/StatistikyDatTester.java
@@ -0,0 +1,33 @@
+package java_spolocna_praca_s_lektorom.webinar11.Zadanie_11_2_4.statistiky;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class StatistikyDatTester
+{
+    public static void main(String[] args)
+    {
+        try (Scanner citacSuboru = new Scanner(new FileReader("statistiky.txt")))
+        {
+            double sucetPrvyStlpec = 0;
+            double sucetDruhyStlpec = 0;
+            int pocet = 0;
+
+            while (citacSuboru.hasNextDouble())
+            {
+                sucetPrvyStlpec += citacSuboru.nextDouble();
+                sucetDruhyStlpec += citacSuboru.nextDouble();
+                pocet++;
+            }
+
+            System.out.println("Priemer cisel v 1. stlpci: " + sucetPrvyStlpec / pocet);
+            System.out.println("Priemer cisel v 2. stlpci: " + sucetDruhyStlpec / pocet);
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
