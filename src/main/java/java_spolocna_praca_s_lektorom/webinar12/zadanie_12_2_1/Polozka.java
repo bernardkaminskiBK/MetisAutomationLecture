609bd8a9042effe07f1f3d39a74bb82a8f819f13
ladislavHubay <hubayladislav7@gmail.com>
2022-09-22 20:46:30 +0200

    pridane webinar12 2x zadanie

---
+++ src/main/java/java_spolocna_praca_s_lektorom/webinar12/zadanie_12_2_1/Polozka.java
@@ -0,0 +1,23 @@
+package java_spolocna_praca_s_lektorom.webinar12.zadanie_12_2_1;
+
+public class Polozka
+{
+    private final Produkt produkt;
+    private final int mnozstvo;
+
+    public Polozka(Produkt produkt, int mnozstvo)
+    {
+        this.produkt = produkt;
+        this.mnozstvo = mnozstvo;
+    }
+
+    public String naformatuj()
+    {
+        return String.format("%-30s%14.2f%14d%14.2f%n", produkt.getNazov(), produkt.getCena(), mnozstvo, getCelkovaCena());
+    }
+
+    public double getCelkovaCena()
+    {
+        return produkt.getCena() * mnozstvo;
+    }
+}
