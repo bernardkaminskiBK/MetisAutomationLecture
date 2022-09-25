07a41245915270a60eeef97b0f79e09ae1d756d7
MartinMesko <alegremato@gmail.com>
2022-09-25 19:33:18 +0200

    new package  and classes created

---
+++ src/main/java/java_spolocna_praca_s_lektorom/webinar22/Zadanie_22_2_1/Problem01/Proble01Tester.java
@@ -0,0 +1,26 @@
+package Problem01;
+
+public class Proble01Tester
+{
+    public static void main(String[] args)
+    {
+        // TODO: dopln chybajucu staticku metodu, ktora bude mat 2 parametre
+        // 1. parameter bude typu Problem1.VystupNaKonzolu
+        // 2. parameter bude typu String
+
+        // String -> std out
+        // 1) vytvor funkcionalny interface
+        // 2) vytvor lambda vyraz
+        VystupNaKonzolu lambda = (String text) -> System.out.println(text);
+
+        // 3 pouzi lambda vyraz na vypisanie textu na konzolu
+        lambda.vypisText("Java");
+        lambda.vypisText("Python");
+        lambda.vypisText("C#");
+    }
+}
+
+interface VystupNaKonzolu
+{
+    void vypisText(String text);
+}
