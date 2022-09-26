499a76b43364ebffa703c58c48344d83ce6fef0d
MartinMesko <alegremato@gmail.com>
2022-09-26 20:35:08 +0200

    class StudentTestJunit renamed

--- src/test/java/java_spolocna_praca_s_lektorom/webinar07/StudentTestJunit.java
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar07/StudentTestJunit.java
@@ -2,7 +2,7 @@ package java_spolocna_praca_s_lektorom.webinar07;
 
 import org.junit.jupiter.api.Test;
 
-public class StudentTestJUnit {
+public class StudentTestJunit {
 
     @Test
     public void studentTest()
