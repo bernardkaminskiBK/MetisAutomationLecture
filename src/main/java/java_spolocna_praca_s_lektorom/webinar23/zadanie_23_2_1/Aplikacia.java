97c882f8620a6d918943ca3ad7b9cb8b13ad6aa9
ladislavHubay <hubayladislav7@gmail.com>
2022-09-26 13:54:58 +0200

    pridany webinar23 6x zadanie

---
+++ src/main/java/java_spolocna_praca_s_lektorom/webinar23/zadanie_23_2_1/Aplikacia.java
@@ -0,0 +1,20 @@
+package java_spolocna_praca_s_lektorom.webinar23.zadanie_23_2_1;
+
+import javax.swing.*;
+
+public class Aplikacia
+{
+    public static void main(String[] args)
+    {
+        Runnable lambda = () -> {
+            JFrame okno = new JFrame();
+
+            okno.setTitle("Grafika 01");
+            okno.setSize(1200, 800);
+            okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            okno.setVisible(true);
+        };
+
+        SwingUtilities.invokeLater(lambda);
+    }
+}
