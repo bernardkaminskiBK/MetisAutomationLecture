779224a4e8f0d5e919241ec474ad000a91352a33
1katka1 <katka981@gmail.com>
2022-09-28 22:19:52 +0200

    REFACTORING

--- src/main/java/java_spolocna_praca_s_lektorom/webinar03/Zadanie_3_2_1/PrikladParsovanieCisel2.java
+++ src/main/java/java_spolocna_praca_s_lektorom/webinar03/Zadanie_3_2_1/PrikladParsovanieCisel2.java
@@ -8,22 +8,33 @@ public class PrikladParsovanieCisel2 {
         String cisloText = "" + cislo;
         String cisloText2 = String.valueOf(cislo);
 
+        System.out.println(cisloText);
+        System.out.println(cisloText2);
+
         // StringBuffer  - pouzivame na vytvaranie velkeho retazce z tisisov mensich casti
         //               - velmi efektivne
         //               - vieme triedu pouzit v ramci viacvlakonoveho prostredia
         StringBuffer buffer = new StringBuffer();
+        buffer.append("text1");
+        buffer.append("text2");
+        buffer.append("text3");
+        buffer.append("text4");
 
+        String vystup1 = buffer.toString();
+        System.out.println(vystup1);
         // StringBuilder - pouzivame na vytvaranie velkeho retazce z tisisov mensich casti
         //               - velmi efektivne
         //               - vieme triedu pouzit len v ramci jednovlaknoveho prostredia
         StringBuilder builder = new StringBuilder();
+
+        builder.append("nejaky text1");
+        builder.append("nejaky text2");
+        builder.append("nejaky text3");
+        builder.append("nejaky text4");
         builder.append(cislo);
-        builder.append("najeky text");
-        builder.append("najeky text");
-        builder.append("najeky text");
-        builder.append("najeky text");
-        builder.append("najeky text");
-        String vystup = builder.toString();
-        System.out.println(vystup);
+
+        String vystup2 = builder.toString();
+        System.out.println(vystup2);
     }
 }
+
