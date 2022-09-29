f082565e55aa8e1d9bc324e0f7e9601791614aa6
MartinMesko <alegremato@gmail.com>
2022-09-29 10:42:33 +0200

    new Test class in Test package 7_2_4 created

--- src/test/java/java_spolocna_praca_s_lektorom/webinar07/Zadanie_7_2_4/InicializacnyBlokTest.java
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar07/Zadanie_7_2_4/InicializacnyBlokTest.java
@@ -1,6 +1,26 @@
 package java_spolocna_praca_s_lektorom.webinar07.Zadanie_7_2_4;
+import java_spolocna_praca_s_lektorom.webinar07.Zadanie_7_2_4.init_blok.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * ?? Neviem, či je to správne ??
+ */
 public class InicializacnyBlokTest {
 
-
+    @ParameterizedTest
+    @CsvSource(value = {"2,2"})
+    void inicializacnyBlokTest(int actualResult, int expectedResult)
+    {
+        InicializacnyBlok inicializacnyBlok = new InicializacnyBlok();
+        InicializacnyBlok inicializacnyBlok1 = new InicializacnyBlok();
+        actualResult = InicializacnyBlok.getPocetVytvorenychObjektov();
+        assertEquals(expectedResult, actualResult);
+    }
 }
