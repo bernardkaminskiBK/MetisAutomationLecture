9e458ffdb9adfbb8008bc536bb7300a3f8846541
MartinMesko <alegremato@gmail.com>
2022-09-30 10:37:39 +0200

    refactoring

--- src/main/java/java_spolocna_praca_s_lektorom/webinar09/Zadanie_9_2_3/DvojrozmerneGrafickeUtvaryTester.java
+++ src/main/java/java_spolocna_praca_s_lektorom/webinar09/Zadanie_9_2_3/DvojrozmerneGrafickeUtvaryTester.java
@@ -1,4 +1,4 @@
-package java_spolocna_praca_s_lektorom.webinar09.Zadanie_9_2_3.dedicnost_navrh_c1_PackagePrivateModifikatorPristupu;
+package java_spolocna_praca_s_lektorom.webinar09.Zadanie_9_2_3;
 
 public class DvojrozmerneGrafickeUtvaryTester
 {
