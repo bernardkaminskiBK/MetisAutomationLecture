3be54e2c4f0e88405039fa6904d321187cead89e
MartinMesko <alegremato@gmail.com>
2022-10-03 12:42:01 +0200

    refactoring

--- src/test/java/java_spolocna_praca_s_lektorom/webinar11/Zadanie_11_2_1/CitacMnozinyDatTest.java
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar11/Zadanie_11_2_1/CitacMnozinyDatTest.java
@@ -1,29 +1,37 @@
 package java_spolocna_praca_s_lektorom.webinar11.Zadanie_11_2_1;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
+import java.io.File;
 import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 class CitacMnozinyDatTest {
 
-    private AnalyzatorDat analyzatorDat;
-
-    @BeforeEach
-    public void setUp()
+    @Test
+    void readFile()
     {
-        analyzatorDat = new AnalyzatorDat();
+        File file = new File("data.txt");
+        assertTrue(file.exists());
+    }
+    
+    @ParameterizedTest
+    @CsvSource(value = {"121.65,data.txt", "120.65,data2.txt", "130,data3.txt"})
+    void citajSuborTest(double expectedResult, String filepath) throws IOException, NespravnyFormatDat {
+        CitacMnozinyDat citac = new CitacMnozinyDat();
+        citac.citajSubor(filepath);
+        double[] data = citac.citajSubor(filepath);
+        double sucet = 0;
+
+        for (double cislo : data)
+        {
+            sucet += cislo;
+        }
+        double actualResult = sucet;
+        assertEquals(expectedResult,actualResult);
+
     }
 
-    @ParameterizedTest
-    @CsvSource()
-    void citacTest(double expectedResult) throws IOException, NespravnyFormatDat {
-        CitacMnozinyDat citac = new CitacMnozinyDat();
-        double[] actual = citac.citajSubor("src/main/java/java_spolocna_praca_s_lektorom/" +
-                "webinar11/Zadanie_11_2_4/statistiky.txt");
-        Assertions.assertEquals(expectedResult,actual);
-    }
 }
