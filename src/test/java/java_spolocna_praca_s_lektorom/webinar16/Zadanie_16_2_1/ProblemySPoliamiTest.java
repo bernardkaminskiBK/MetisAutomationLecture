c9f321489d4f7424b2d50232ef7a060d19c83e95
MartinMesko <alegremato@gmail.com>
2022-10-04 10:33:36 +0200

    new three test classes created

---
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar16/Zadanie_16_2_1/ProblemySPoliamiTest.java
@@ -0,0 +1,43 @@
+package java_spolocna_praca_s_lektorom.webinar16.Zadanie_16_2_1;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProblemySPoliamiTest {
+
+
+    @ParameterizedTest
+    @CsvSource(value = {"Game of thrones,20,2,Game of thrones", "Assassins creed,10,0.5,Assassins creed",
+            "JUnit5,12.5,0.7,JUnit5"})
+    void getNazovTest(String nazov, double cena, double vaha, String expectedResult)
+    {
+        Vyrobok vyrobok = new Vyrobok(nazov, cena, vaha);
+        String actualResult = vyrobok.getNazov();
+        assertEquals(expectedResult,actualResult);
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(value = {"Game of thrones,20,2,20", "Assassins creed,10,0.5,10",
+            "JUnit5,12.5,0.7,12.5"})
+    void getCenaTest(String nazov, double cena, double vaha, double expectedResult)
+    {
+        Vyrobok vyrobok = new Vyrobok(nazov, cena, vaha);
+        double actualResult = vyrobok.getCena();
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"Game of thrones,20,2,2", "Assassins creed,10,0.5,0.5",
+            "JUnit5,12.5,0.7,0.7"})
+    void getVahaTest(String nazov, double cena, double vaha, double expectedResult)
+    {
+        Vyrobok vyrobok = new Vyrobok(nazov, cena, vaha);
+        double actualResult = vyrobok.getVaha();
+        assertEquals(expectedResult,actualResult);
+    }
+
+
+
+}
