2a93632d60e893ccf667eeace61bd20d14752429
ladislavHubay <hubayladislav7@gmail.com>
2022-10-05 12:29:53 +0200

    webinar10 zadanie 20.2.2 zmeneby setMultiSelectionEnabled na true

--- src/main/java/java_spolocna_praca_s_lektorom/webinar10/zadanie_10_2_2/KomponentaJFileChooserDemo.java
+++ src/main/java/java_spolocna_praca_s_lektorom/webinar10/zadanie_10_2_2/KomponentaJFileChooserDemo.java
@@ -2,7 +2,6 @@ package java_spolocna_praca_s_lektorom.webinar10.zadanie_10_2_2;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import javax.swing.filechooser.FileSystemView;
 import java.io.File;
 
 public class KomponentaJFileChooserDemo
@@ -12,29 +11,19 @@ public class KomponentaJFileChooserDemo
         //JFileChooser navigator = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
         JFileChooser navigator = new JFileChooser();
 
-
-
         navigator.setFileSelectionMode(JFileChooser.FILES_ONLY);
         navigator.setDialogTitle("Vyber nejaky obrazok");
         navigator.setMultiSelectionEnabled(true);
-        navigator.setAcceptAllFileFilterUsed(true);        // zapne filter *.* (moznoje to default hodnota)
-
-
+        navigator.setAcceptAllFileFilterUsed(false);        // zapne filter *.* (moznoje to default hodnota)
 
         FileNameExtensionFilter filter = new FileNameExtensionFilter("Obrazky jpg a png", "jpg", "png");
         navigator.addChoosableFileFilter(filter);
 
-
-
         int stav = navigator.showOpenDialog(null);
-
-
 
         if (stav == JFileChooser.APPROVE_OPTION)
         {
             File[] oznaceneSubory = navigator.getSelectedFiles();
-
-
 
             for (File subor : oznaceneSubory)
             {
