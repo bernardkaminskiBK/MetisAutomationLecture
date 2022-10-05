a2da7de160b169fc153ceca09047302b13600d18
michal kasan <michal.kasan@metis.academy>
2022-10-05 11:01:25 +0200

    1st commit:typo:Ocakvana->Ocakavana

--- src/main/java/java_spolocna_praca_s_lektorom/webinar09/Zadanie_9_2_2/volanie_konstruktora/Podtrieda.java
+++ src/main/java/java_spolocna_praca_s_lektorom/webinar09/Zadanie_9_2_2/volanie_konstruktora/Podtrieda.java
@@ -3,5 +3,7 @@ package java_spolocna_praca_s_lektorom.webinar09.Zadanie_9_2_2.volanie_konstrukt
 public class Podtrieda extends Nadtrieda
 {
     // automaticky sa vytvori tzv. defaultny konstruktor
-    // public Podtrieda () { super(); }
+     public Podtrieda () {
+         super();
+     }
 }
