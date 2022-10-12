6a9fb8a7b2fc29247e50824aca6925791603aea1
MartinMesko <alegremato@gmail.com>
2022-10-12 11:21:22 +0200

    New subpackage Zadanie_21_2_1 with test class created

---
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar21/Zadanie_21_2_2/Oslovovac2Test.java
@@ -0,0 +1,21 @@
+package java_spolocna_praca_s_lektorom.webinar21.Zadanie_21_2_2;
+
+import java_spolocna_praca_s_lektorom.webinar21.zadanie_21_2_2.Oslovovac2;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import utils.StorePrintStream;
+
+public class Oslovovac2Test {
+
+    @Test
+    void oslovovac2Test()
+    {
+        System.setOut(new StorePrintStream(System.out));
+        Oslovovac2.main(new String[0]);
+
+        assertTrue(StorePrintStream.printlnList.get(0).contains("Ahoj!"));
+        assertTrue(StorePrintStream.printlnList.get(1).contains("Nazdar!"));
+        assertTrue(StorePrintStream.printlnList.get(2).contains("Dobry den!"));
+        assertEquals(3, StorePrintStream.printlnList.size());
+    }
+}
