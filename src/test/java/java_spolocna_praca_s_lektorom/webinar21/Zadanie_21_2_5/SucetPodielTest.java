2f18d9cbe3a32daa578e4e7ea6006ad5d6e7ef4a
MartinMesko <alegremato@gmail.com>
2022-10-12 12:57:37 +0200

    New test classes created.

--- src/test/java/java_spolocna_praca_s_lektorom/webinar21/Zadanie_21_2_5/SucetPodielTest.java
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar21/Zadanie_21_2_5/SucetPodielTest.java
@@ -2,6 +2,8 @@ package java_spolocna_praca_s_lektorom.webinar21.Zadanie_21_2_5;
 
 import java_spolocna_praca_s_lektorom.webinar21.zadanie_21_2_5.SucetPodielTester;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import utils.StorePrintStream;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,5 +17,35 @@ public class SucetPodielTest {
 
         assertTrue(StorePrintStream.printlnList.get(0).contains("Sucet: 30"));
         assertEquals(2,StorePrintStream.printlnList.size());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"10,2,5", "20,4,5", "30,2,15"})
+    void vypocetPodielTest(double actualNumber1, double actualNumber2, int expectedResult)
+    {
+        Podiel podiel = (double cislo1, double cislo2) -> cislo1 / cislo2;
+        double actualResult = podiel.zrealizujpodiel(actualNumber1,actualNumber2);
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"2,2,4", "3,7,10", "4,10,14"})
+    void vypocetSucetTest(int actualNumber1, int actualNumber2, int expectedResult)
+    {
+        Sucet sucet = Integer::sum;
+        int actualResult = sucet.zrealizujSucet(actualNumber1,actualNumber2);
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @FunctionalInterface
+    interface Sucet
+    {
+        int zrealizujSucet(int cislo1, int cislo2);
+    }
+
+    @FunctionalInterface
+    interface Podiel
+    {
+        double zrealizujpodiel(double cislo1, double cislo2);
     }
 }
