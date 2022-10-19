587029796dac12bb334d816c3b36d5191e5b955e
MartinMesko <alegremato@gmail.com>
2022-10-19 21:12:48 +0200

    new test method added.

--- src/test/java/java_spolocna_praca_s_lektorom/webinar22/Zadanie22_2_6/KrajinyTest.java
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar22/Zadanie22_2_6/KrajinyTest.java
@@ -1,7 +1,10 @@
 package java_spolocna_praca_s_lektorom.webinar22.Zadanie22_2_6;
 
+import java_spolocna_praca_s_lektorom.webinar22.Zadanie_22_2_6.b.KrajinyStream_v2;
 import org.junit.jupiter.api.Test;
+import utils.StorePrintStream;
 import java.io.File;
+import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class KrajinyTest {
@@ -13,4 +16,19 @@ public class KrajinyTest {
         assertTrue(file.exists());
     }
 
+    @Test
+    void krajinaTest() throws IOException {
+        System.setOut(new StorePrintStream(System.out));
+        KrajinyStream_v2.main(new String[0]);
+
+        assertTrue(StorePrintStream.printList.get(0).contains("Rakusko"));
+    }
+
+
 }
+
+
+
+
+
+
