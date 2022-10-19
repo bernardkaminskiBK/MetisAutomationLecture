eb11b983b805f8ee5479b7f08c023bd7f97ac001
MartinMesko <alegremato@gmail.com>
2022-10-19 13:14:21 +0200

    refactoring

--- src/test/java/java_spolocna_praca_s_lektorom/webinar22/Zadanie22_2_4/HracTest.java
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar22/Zadanie22_2_4/HracTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class HracTest {
     @ParameterizedTest
-    @CsvSource(value = {"Juraj,Jánošík,10,Juraj", "Ištván,Ferencvároš,18,Ištván","Monika,Chochlíková,20,Monika","Tomáš,Tadlánek,25,Tomáš"})
+    @CsvSource(value = {"Juraj,Makarón,10,Juraj", "Peter,Petrovič,18,Peter","Monika,Chochlíková,20,Monika","Tomáš,Tadlánek,25,Tomáš"})
     void hracGetMenoTest(String meno, String priezvisko, int pocetGolov, String expectedResult)
     {
         Hrac hrac = new Hrac(meno, priezvisko,pocetGolov);
@@ -20,7 +20,7 @@ public class HracTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"Juraj,Jánošík,10,Jánošík", "Ištván,Ferencvároš,18,Ferencvároš","Monika,Chochlíková,20,Chochlíková","Tomáš,Tadlánek,25,Tadlánek"})
+    @CsvSource(value = {"Juraj,Makarón,10,Makarón", "Peter,Petrovič,18,Petrovič","Monika,Chochlíková,20,Chochlíková","Tomáš,Tadlánek,25,Tadlánek"})
     void hracPriezviskoTest(String meno, String priezvisko, int pocetGolov, String expectedResult)
     {
         Hrac hrac = new Hrac(meno, priezvisko,pocetGolov);
@@ -29,7 +29,7 @@ public class HracTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"Juraj,Jánošík,10,10", "Ištván,Ferencvároš,18,18","Monika,Chochlíková,20,20","Tomáš,Tadlánek,25,25"})
+    @CsvSource(value = {"Juraj,Makarón,10,10", "Peter,Petrovič,18,18","Monika,Chochlíková,20,20","Tomáš,Tadlánek,25,25"})
     void hracGetPocetGolovTest(String meno, String priezvisko, int pocetGolov, int expectedResult)
     {
         Hrac hrac = new Hrac(meno, priezvisko,pocetGolov);
@@ -43,40 +43,24 @@ public class HracTest {
     {
         List<Hrac> hraci = new ArrayList<>();
         hraci.add(new Hrac("Maroš", "Molnár", 26));
-        hraci.add(new Hrac("Juraj", "Jánošík", 29));
+        hraci.add(new Hrac("Juraj", "Dakarón", 29));
         hraci.add(new Hrac("Peter", "Sagan", 23));
         hraci.add(new Hrac("Juraj", "Sagan", 23));
-        hraci.add(new Hrac("Peter", "Molnár", 23));
+        hraci.add(new Hrac("Peter", "Lolnár", 23));
 
         Comparator<Hrac> podlaPoctuGolov = Comparator.comparing(Hrac::getPocetGolov)
                 .thenComparing(Hrac::getPriezvisko)
                 .thenComparing(Hrac::getMeno)
                 .reversed();
 
+
         hraci.stream()
                 .sorted(podlaPoctuGolov)
                 .map(hraciSk -> hraciSk.getMeno() + " " + hraciSk.getPriezvisko() + " " + hraciSk.getPocetGolov())
                 .forEach(System.out::println);
+
+
     }
 
-//    @ParameterizedTest
-//    @CsvSource(value = {"Peter,Sagan,12", "Maroš,Molnár,17","Martin,Kralovič,10"})
-//    void triedenieHracov2Test(String meno, String priezvisko, int pocetGolov)
-//    {
-//        List<Hrac> hraci = new ArrayList<>();
-//        hraci.add(new Hrac(meno, priezvisko, pocetGolov));
-//        hraci.add(new Hrac(meno, priezvisko, pocetGolov));
-//        hraci.add(new Hrac(meno, priezvisko, pocetGolov));
-//
-//
-//        Comparator<Hrac> podlaPoctuGolov = Comparator.comparing(Hrac::getPocetGolov)
-//                .thenComparing(Hrac::getPriezvisko)
-//                .thenComparing(Hrac::getMeno)
-//                ;
-//
-//        hraci.stream()
-//                .sorted(podlaPoctuGolov)
-//                .map(hraciSk -> hraciSk.getMeno() + " " + hraciSk.getPriezvisko() + " " + hraciSk.getPocetGolov())
-//                .forEach(System.out::println);
-//    }
+
 }
