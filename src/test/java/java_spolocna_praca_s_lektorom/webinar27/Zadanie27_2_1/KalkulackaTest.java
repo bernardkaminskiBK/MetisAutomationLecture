dbb552211ceb71b5fd3c87e68d73adf9b72c111d
MartinMesko <alegremato@gmail.com>
2022-10-20 10:31:36 +0200

    New package webinar27 with test class and test method added.

---
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar27/Zadanie27_2_1/KalkulackaTest.java
@@ -0,0 +1,18 @@
+package java_spolocna_praca_s_lektorom.webinar27.Zadanie27_2_1;
+
+import java_spolocna_praca_s_lektorom.webinar27.Zadanie_27_2_1.Kalkulacka;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class KalkulackaTest {
+    @ParameterizedTest
+    @CsvSource(value = {"1,1,2", "2,2,4", "5,5,10"})
+    void kalkulackaTest(int a, int b, int expectedResult)
+    {
+        Kalkulacka kalkulacka = new Kalkulacka();
+        int actualResult = kalkulacka.spocitaj(a, b);
+        assertEquals(expectedResult,actualResult, "Result should be : " + expectedResult);
+
+    }
+}
