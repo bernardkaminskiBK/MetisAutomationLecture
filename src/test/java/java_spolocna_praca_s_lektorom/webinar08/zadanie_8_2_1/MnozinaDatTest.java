8090c3b386cf26a7d934204906040c3e3340d19d
MartinMesko <alegremato@gmail.com>
2022-10-21 12:57:47 +0200

    New test methods added.

--- src/test/java/java_spolocna_praca_s_lektorom/webinar08/zadanie_8_2_1/MnozinaDatTest.java
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar08/zadanie_8_2_1/MnozinaDatTest.java
@@ -1,6 +1,18 @@
 package java_spolocna_praca_s_lektorom.webinar08.zadanie_8_2_1;
 
-class MnozinaDatTest
-{
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+public class MnozinaDatTest
+{
+    @ParameterizedTest
+    @CsvSource(value = {"1000,1000", "1200,1200", "2000,2000"})
+    void getAktualnyZostatokTest(double zostatok, double expectedResult)
+    {
+        BankovyUcet bankovyUcet = new BankovyUcet(zostatok);
+        double actualResult = bankovyUcet.getAktualnyZostatok();
+        Assertions.assertEquals(expectedResult,actualResult);
+    }
 }
+
