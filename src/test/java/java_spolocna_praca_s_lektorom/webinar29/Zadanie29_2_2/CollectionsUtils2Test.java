9dc0534586692b0681abc501c36509197583c8b5
MartinMesko <alegremato@gmail.com>
2022-10-21 10:47:10 +0200

    CollectionsUtils2Test renamed.

---
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar29/Zadanie29_2_2/CollectionsUtils2Test.java
@@ -0,0 +1,12 @@
+package java_spolocna_praca_s_lektorom.webinar29.Zadanie29_2_2;
+
+import org.junit.jupiter.api.Test;
+
+public class CollectionsUtils2Test {
+
+    @Test
+    void collectionTest()
+    {
+
+    }
+}
