4b976ceca61551be0ceea67e3f6edcc63f351591
ladislavHubay <hubayladislav7@gmail.com>
2022-10-24 10:31:11 +0200

    rozdelenie zadani ProFun

---
+++ src/main/java/profun/Lekcia04/Zadanie_4_3_3/PostupnostCisel.java
@@ -0,0 +1,21 @@
+package profun.Lekcia04.Zadanie_4_3_3;
+
+public class PostupnostCisel
+{
+    public static void main(String[] args)
+    {
+        System.out.println(vypisPostupnostCisel());
+    }
+
+    public static String vypisPostupnostCisel() {
+        int cislo = 1;
+        String vysledok = "";
+
+        while (cislo <= 10)
+        {
+            vysledok += cislo++  + "\t";
+        }
+
+        return vysledok;
+    }
+}
