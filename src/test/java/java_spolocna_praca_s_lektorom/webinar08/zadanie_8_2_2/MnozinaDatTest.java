51258554663d47693fd0c3858365c13806156ec9
MartinMesko <alegremato@gmail.com>
2022-10-31 13:20:05 +0100

    New test methods added.

--- src/test/java/java_spolocna_praca_s_lektorom/webinar08/zadanie_8_2_2/MnozinaDatTest.java
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar08/zadanie_8_2_2/MnozinaDatTest.java
@@ -1,22 +1,30 @@
 package java_spolocna_praca_s_lektorom.webinar08.zadanie_8_2_2;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class MnozinaDatTest
 {
-    //test synchronizacie s GitHub
+
     @ParameterizedTest
-    @CsvSource(value = {"Eurovka,1.0", "Centovka,0.01", "Patcentovka,0.05"})
-    void mincaTest(String nazov, double hodnota)
+    @CsvSource(value = {"Desatcentovka,0.1,Desatcentovka", "Dvadsatcentovka,0.2,Dvadsatcentovka",
+    "Eurovka,1.0,Eurovka"})
+    void mincaGetNazovTest(String nazov, double hodnota, String expectedResult)
     {
         Minca minca = new Minca(nazov, hodnota);
-        MnozinaDat mnozinaDat = new MnozinaDat();
-        mnozinaDat.pridajPrvok(minca);
-        mnozinaDat.pridajPrvok(minca);
-        mnozinaDat.pridajPrvok(minca);
-        double actualResult = mnozinaDat.getMaximum().getHodnota();
-        System.out.println(actualResult);
-
+        String actualResult = minca.getNazov();
+        Assertions.assertEquals(expectedResult,actualResult);
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"Desatcentovka,0.1,0.1", "Dvadsatcentovka,0.2,0.2",
+            "Eurovka,1.0,1.0"})
+    void mincaGetHodnotaTest(String nazov, double hodnota, double expectedResult)
+    {
+        Minca minca = new Minca(nazov, hodnota);
+        double actualResult = minca.getHodnota();
+        Assertions.assertEquals(expectedResult,actualResult);
+    }
+
 }
