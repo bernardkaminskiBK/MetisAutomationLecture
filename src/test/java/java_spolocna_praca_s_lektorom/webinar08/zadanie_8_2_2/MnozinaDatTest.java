231730478c2f9c3ad710b0af6c7380637d3e0726
MartinMesko <alegremato@gmail.com>
2022-10-31 14:12:19 +0100

    New test methods added.

--- src/test/java/java_spolocna_praca_s_lektorom/webinar08/zadanie_8_2_2/MnozinaDatTest.java
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar08/zadanie_8_2_2/MnozinaDatTest.java
@@ -1,8 +1,10 @@
 package java_spolocna_praca_s_lektorom.webinar08.zadanie_8_2_2;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
 
 class MnozinaDatTest
 {
@@ -25,6 +27,44 @@ class MnozinaDatTest
         Minca minca = new Minca(nazov, hodnota);
         double actualResult = minca.getHodnota();
         Assertions.assertEquals(expectedResult,actualResult);
+    }
+
+    @ParameterizedTest
+    @CsvSource("Desatcentovka,0.1")
+    void mincaPridajPrvokTest(String nazov, double hodnota)
+    {
+        Minca minca = new Minca(nazov, hodnota);
+        MnozinaDat mnozinaDat = new MnozinaDat();
+        mnozinaDat.pridajPrvok(minca);
+        Assertions.assertEquals(0.1, minca.getHodnota());
+    }
+
+    @Test
+    void mincaGetMinimum()
+    {
+        Minca minca1 = new Minca("Eurovka", 1.0);
+        Minca minca2 = new Minca("Centovka", 0.01);
+        Minca minca3 = new Minca("Patcentovka", 0.05);
+
+        MnozinaDat mnozinaDat = new MnozinaDat();
+        mnozinaDat.pridajPrvok(minca1);
+        mnozinaDat.pridajPrvok(minca2);
+        mnozinaDat.pridajPrvok(minca3);
+        Assertions.assertEquals(0.01, mnozinaDat.getMinimum().getHodnota());
+    }
+
+    @Test
+    void mincaGetMaximum()
+    {
+        Minca minca1 = new Minca("Eurovka", 1.0);
+        Minca minca2 = new Minca("Centovka", 0.01);
+        Minca minca3 = new Minca("Patcentovka", 0.05);
+
+        MnozinaDat mnozinaDat = new MnozinaDat();
+        mnozinaDat.pridajPrvok(minca1);
+        mnozinaDat.pridajPrvok(minca2);
+        mnozinaDat.pridajPrvok(minca3);
+        Assertions.assertEquals(1.0, mnozinaDat.getMaximum().getHodnota());
     }
 
 }
