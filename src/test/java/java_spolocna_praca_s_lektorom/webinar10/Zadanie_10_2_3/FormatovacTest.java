5352d2b126e5ecedae540b3ec3396b1be704cfc5
MartinMesko <alegremato@gmail.com>
2022-11-02 13:04:14 +0100

    Test class refactored

--- src/test/java/java_spolocna_praca_s_lektorom/webinar10/Zadanie_10_2_3/FormatovacTest.java
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar10/Zadanie_10_2_3/FormatovacTest.java
@@ -1,8 +1,8 @@
 package java_spolocna_praca_s_lektorom.webinar10.Zadanie_10_2_3;
 
-
 import org.junit.jupiter.api.Test;
-import java.io.File;
+import java.io.*;
+
 
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -11,8 +11,15 @@ public class FormatovacTest {
     @Test
     void formatovacIOTest() {
 
-        File file = new File("subor.txt");
+        File file = new File("vystup.txt");
         assertTrue(file.exists());
-
     }
+
+//    @Test
+//    void formatovacTest() throws IOException {
+//
+//        System.setOut(new StorePrintStream(System.out));
+//        Formatovac.main(new String[0]);
+//
+//    }
 }
