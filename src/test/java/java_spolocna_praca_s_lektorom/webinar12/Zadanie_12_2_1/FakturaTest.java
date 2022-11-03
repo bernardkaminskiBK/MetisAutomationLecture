0835329f193524f55070c48a930a90718b7f9208
MartinMesko <alegremato@gmail.com>
2022-11-03 20:00:07 +0100

    New test method added.

--- src/test/java/java_spolocna_praca_s_lektorom/webinar12/Zadanie_12_2_1/FakturaTest.java
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar12/Zadanie_12_2_1/FakturaTest.java
@@ -1,6 +1,8 @@
 package java_spolocna_praca_s_lektorom.webinar12.Zadanie_12_2_1;
 
 import java_spolocna_praca_s_lektorom.webinar12.zadanie_12_2_1.Adresa;
+import java_spolocna_praca_s_lektorom.webinar12.zadanie_12_2_1.Faktura;
+import java_spolocna_praca_s_lektorom.webinar12.zadanie_12_2_1.Produkt;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -18,6 +20,38 @@ public class FakturaTest {
                 Prievozská 34
                 812 05 Bratislava""";
         Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"Hriankovac,12,Hriankovac", "Radio,120,Radio", "Satelit,78,Satelit"})
+    void produktGetNazovTest(String nazov, double cena, String expectedResult)
+    {
+        Produkt produkt = new Produkt(nazov, cena);
+        String actualResult = produkt.getNazov();
+        Assertions.assertEquals(expectedResult,actualResult);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"Hriankovac,12.2,12.2", "Radio,120.7,120.7", "Satelit,78.8,78.8"})
+    void produktGetCenaTest(String nazov, double cena, double expectedResult)
+    {
+        Produkt produkt = new Produkt(nazov, cena);
+        double actualResult = produkt.getCena();
+        Assertions.assertEquals(expectedResult,actualResult);
+    }
+
+    /**
+     *
+     * Treba ešte dorobiť !!!
+     */
+    @ParameterizedTest
+    @CsvSource(value = {"Hriankovac,12.2", "Radio,120.7", "Satelit,78.8"})
+    void fakturaTest(String nazov, double cena)
+    {
+        Adresa adresa = new Adresa("Firma XYZ", "Horna 5", "12345", "Horna Dolan");
+        Faktura faktura = new Faktura(adresa);
+        faktura.pridaj(new Produkt(nazov,cena), 10);
+        System.out.println(faktura.naformatuj());
     }
 
 }
