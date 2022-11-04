b594137a64668d36e3001ae7d63f759438281063
MartinMesko <alegremato@gmail.com>
2022-11-04 16:48:17 +0100

    New test methods added.

--- src/test/java/java_spolocna_praca_s_lektorom/webinar17/Zadanie17_2_1/ZasielkaTest.java
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar17/Zadanie17_2_1/ZasielkaTest.java
@@ -1,11 +1,17 @@
 package java_spolocna_praca_s_lektorom.webinar17.Zadanie17_2_1;
 
+import java_spolocna_praca_s_lektorom.webinar17.zadanie_17_2_1.Sklad;
 import java_spolocna_praca_s_lektorom.webinar17.zadanie_17_2_1.Vyrobok;
+import java_spolocna_praca_s_lektorom.webinar17.zadanie_17_2_1.Zasielka;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import static org.assertj.core.api.Assertions.*;
 
 public class ZasielkaTest {
+
+    Zasielka zasielka = new Zasielka();
 
     @ParameterizedTest
     @CsvSource(value = {"Kalkulačka,2.50,0.2,Kalkulačka", "Rádio,20,1.5,Rádio", "Satelit,120,20,Satelit"})
@@ -32,5 +38,23 @@ public class ZasielkaTest {
         Vyrobok vyrobok = new Vyrobok(nazov, cena, vaha);
         double actualResult = vyrobok.getVaha();
         Assertions.assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    void pridajZasielkuTest()
+    {
+
+        zasielka.pridaj(Sklad.dvere);
+        zasielka.pridaj(Sklad.dlazobnaKocka);
+        assertThat(zasielka).contains(Sklad.dlazobnaKocka, Sklad.dvere);
+    }
+
+    @Test
+    void nahradVyrobokTest()
+    {
+        zasielka.pridaj(Sklad.okno);
+        zasielka.pridaj(Sklad.dlazobnaKocka);
+        zasielka.nahrad(Sklad.dlazobnaKocka, Sklad.dvere);
+        assertThat(zasielka).containsExactly(Sklad.okno, Sklad.dvere);
     }
 }
