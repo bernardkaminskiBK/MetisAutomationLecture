88b21fdc7035e78c52e2b43660777b2320dac5c7
MartinMesko <alegremato@gmail.com>
2022-11-09 11:55:43 +0100

    Refactoring

--- src/test/java/java_spolocna_praca_s_lektorom/webinar18/Zadanie_18_2_2/HelpDestTest.java
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar18/Zadanie_18_2_2/HelpDestTest.java
@@ -2,6 +2,8 @@ package java_spolocna_praca_s_lektorom.webinar18.Zadanie_18_2_2;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import utils.StorePrintStream;
 
 
@@ -14,5 +16,21 @@ public class HelpDestTest {
         HelpDesk.main(new String[0]);
         Assertions.assertTrue(StorePrintStream.printList.get(0).startsWith("Pista"));
     }
+
+//    @ParameterizedTest
+//    @CsvSource(value = {"Karol"})
+//    void getZakaznik(String expectedResult)
+//    {
+//        Poziadavka zakaznik = new Poziadavka(Zakaznik.KAROL, Kategoria.TLACIAREN);
+//        Assertions.assertEquals(expectedResult, Zakaznik.KAROL.getClass().getName());
+//
+//    }
+
+//    @Test
+//    void zaevidujPoziadavkuTest()
+//    {
+//        Poziadavka poziadavka = new Poziadavka(new Zakaznik("Katka"),Kategoria.TLACIAREN);
+//        Assertions.assertEquals("Katka", poziadavka.getZakaznik().);
+//    }
 
 }
