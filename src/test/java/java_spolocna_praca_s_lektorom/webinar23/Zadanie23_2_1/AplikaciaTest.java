4ddeba0bde5c5015ac5411765495163d676e8d5d
MartinMesko <alegremato@gmail.com>
2022-11-09 19:10:05 +0100

    New package with test class and test method added.

---
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar23/Zadanie23_2_1/AplikaciaTest.java
@@ -0,0 +1,32 @@
+package java_spolocna_praca_s_lektorom.webinar23.Zadanie23_2_1;
+
+import org.junit.jupiter.api.Test;
+import javax.swing.*;
+
+public class AplikaciaTest {
+    static JFrame okno;
+    @Test
+    void oknoTest()
+    {
+        Runnable lambda = () -> {
+            okno = new JFrame();
+
+            okno.setTitle("Grafika 01");
+            okno.setSize(1200, 800);
+            okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            okno.setVisible(true);
+        };
+
+        SwingUtilities.invokeLater(lambda);
+
+        try {
+            Thread.sleep(2000);
+            System.out.println(okno.getTitle());
+            Thread.sleep(2000);
+            okno.setVisible(true);
+            okno.dispose();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
