bf1c93732969a23f8f1967b41b8c225885be1b83
MartinMesko <alegremato@gmail.com>
2022-11-10 14:11:43 +0100

    new test method added.

--- src/test/java/java_spolocna_praca_s_lektorom/webinar18/Zadanie_18_2_2/HelpDestTest.java
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar18/Zadanie_18_2_2/HelpDestTest.java
@@ -3,6 +3,10 @@ package java_spolocna_praca_s_lektorom.webinar18.Zadanie_18_2_2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import utils.StorePrintStream;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import static org.assertj.core.api.Assertions.*;
+
 
 
 public class HelpDestTest {
@@ -27,4 +31,14 @@ public class HelpDestTest {
         Assertions.assertEquals(katka, poziadavka.getZakaznik());
     }
 
+    @Test
+    void spracujPoziadavkuTest()
+    {
+        final Queue<Poziadavka> poziadavky = new ArrayDeque<>();
+        poziadavky.add(new Poziadavka(Zakaznik.KATKA,Kategoria.TLACIAREN));
+        poziadavky.add(new Poziadavka(Zakaznik.KAROL,Kategoria.POCITAC));
+        poziadavky.remove();
+        poziadavky.remove();
+        assertThat(poziadavky).isEmpty();
+    }
 }
