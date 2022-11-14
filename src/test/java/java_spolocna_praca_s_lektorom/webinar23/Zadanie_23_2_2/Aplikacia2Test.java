1649c63efc06446e5ff25445ce5832e5beab7997
MartinMesko <alegremato@gmail.com>
2022-11-14 16:45:10 +0100

    New test method added.

--- src/test/java/java_spolocna_praca_s_lektorom/webinar23/Zadanie_23_2_2/Aplikacia2Test.java
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar23/Zadanie_23_2_2/Aplikacia2Test.java
@@ -5,10 +5,14 @@ import java_spolocna_praca_s_lektorom.webinar23.zadanie_23_2_2.HlavneOkno;
 import org.junit.jupiter.api.Assertions;
 import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import javax.swing.*;
+
 
 
 public class Aplikacia2Test {
     HlavneOkno hlavneokno = new HlavneOkno();
+    JTextArea txaText = new JTextArea();
+    JButton btnKlikni = new JButton("Klik");
 
     @Test
     void getTitleofJFrame()
@@ -38,5 +42,21 @@ public class Aplikacia2Test {
         }
         assertThat(hlavneokno.getSize().width).isEqualTo(1200);
         Assertions.assertEquals(800, hlavneokno.getSize().height);
+
     }
+
+    @Test
+    void isVisibleOrNot()
+    {
+        try {
+            Aplikacia.main(new String[0]);
+            Thread.sleep(500);
+            Assertions.assertTrue(hlavneokno.isVisible());
+            hlavneokno.dispose();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
