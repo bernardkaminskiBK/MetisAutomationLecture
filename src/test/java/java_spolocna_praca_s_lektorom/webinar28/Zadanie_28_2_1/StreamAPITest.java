909a3b6bad21e9f77674131fa89500afa5e2a65e
MartinMesko <alegremato@gmail.com>
2022-11-16 14:05:00 +0100

    New test method added.

--- src/test/java/java_spolocna_praca_s_lektorom/webinar28/Zadanie_28_2_1/StreamAPITest.java
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar28/Zadanie_28_2_1/StreamAPITest.java
@@ -24,6 +24,10 @@ public class StreamAPITest {
         assertThat(streamApi.filterOnlyPositiveNumber(list)).containsOnly(2,4,6);
 
     }
-
-
+    @Test
+    void filterOnlyPositiveNumberStreamApi()
+    {
+        List<Integer> list = List.of(1,-2,3,4,-5,6,-7,8,9);
+        assertThat(StreamApi.filterOnlyPositiveNumberStreamApi(list)).containsOnly(1,3,4,6,8,9);
+    }
 }
