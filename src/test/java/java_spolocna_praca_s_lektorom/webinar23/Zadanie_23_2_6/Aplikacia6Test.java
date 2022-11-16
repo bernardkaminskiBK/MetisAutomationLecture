d2f6519a9965dad47d1204f32132353be7400ac9
MartinMesko <alegremato@gmail.com>
2022-11-16 11:26:06 +0100

    New test method added.

--- src/test/java/java_spolocna_praca_s_lektorom/webinar23/Zadanie_23_2_6/Aplikacia6Test.java
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar23/Zadanie_23_2_6/Aplikacia6Test.java
@@ -3,6 +3,9 @@ package java_spolocna_praca_s_lektorom.webinar23.Zadanie_23_2_6;
 import java_spolocna_praca_s_lektorom.webinar23.zadanie_23_2_6.Aplikacia;
 import java_spolocna_praca_s_lektorom.webinar23.zadanie_23_2_6.HlavneOkno;
 import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -43,6 +46,18 @@ public class Aplikacia6Test {
         try {
             Thread.sleep(100);
             assertThat(hlavneOkno.isVisible()).isTrue();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void closeOprationTest()
+    {
+        Aplikacia.main(new String[0]);
+        try {
+            Thread.sleep(100);
+            assertThat(hlavneOkno.getDefaultCloseOperation()).isEqualTo(JFrame.EXIT_ON_CLOSE);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
