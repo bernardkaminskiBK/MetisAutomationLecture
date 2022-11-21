d93cdf7530b30d264c40c85ae58259c1a1d2feb0
1katka1 <katka981@gmail.com>
2022-11-21 10:44:08 +0100

    renamed file

--- src/main/java/java_spolocna_praca_s_lektorom/webinar03/Zadanie_3_2_1/a/PrikladParsovanieCisel1.java
+++ src/main/java/java_spolocna_praca_s_lektorom/webinar03/Zadanie_3_2_1/a/PrikladParsovanieCisel1.java
@@ -1,6 +1,6 @@
 package java_spolocna_praca_s_lektorom.webinar03.Zadanie_3_2_1.a;
 
-public class PrikladParsovanieCisel {
+public class PrikladParsovanieCisel1 {
     public static void main(String[] args) {
         // String -> Integer
         String data = "123";
