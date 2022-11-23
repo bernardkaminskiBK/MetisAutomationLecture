de3723ae557b9b7a318adc0370522b83cea870b2
bernardkaminskBK <bernardkaminski.bk@gmail.com>
2022-11-23 13:06:37 +0100

    prepared example for InputOutputStreamHelper class

--- src/test/java/profun_spolocna_praca_s_lektorom/Lekcia4/zadanie2/StavNaUcteTest.java
+++ src/test/java/profun_spolocna_praca_s_lektorom/Lekcia4/zadanie2/StavNaUcteTest.java
@@ -1,5 +1,6 @@
 package profun_spolocna_praca_s_lektorom.Lekcia4.zadanie2;
 
+import base.InputOutputStreamHelper;
 import org.junit.jupiter.api.Assertions;
 import profun_spolocna_praca_s_lektorom.Lekcia04.zadanie2.StavNaUcteTester;
 import utils.StorePrintStream;
@@ -8,30 +9,37 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.Scanner;
 
-
-class StavNaUcteTest {
+/*
+* Priklad na ucinost refactoringu z StorePrintStremu na InputOutputStreamHelper
+* */
+class StavNaUcteTest extends InputOutputStreamHelper {
 
     @Test
     public void limitNaKreditkeTest() {
-        System.setOut(new StorePrintStream(System.out));
+//        System.setOut(new StorePrintStream(System.out));
 
         String input = "1000\n" + "1000\n" + "500\n" + "1000\n" + "100\n" + "-1\n";
-        StavNaUcteTester.zistiStavNaUcte(new Scanner(input));
+//        StavNaUcteTester.zistiStavNaUcte(new Scanner(input));
 
-        System.out.println("\n\nactual result print: " + StorePrintStream.printlnList);
-        System.out.println("actual result printf: " + StorePrintStream.printfList);
+//        System.out.println("\n\nactual result print: " + StorePrintStream.printlnList);
+//        System.out.println("actual result printf: " + StorePrintStream.printfList);
+//
+//        List<String> printList = StorePrintStream.printlnList;
+//        List<String> printfList = StorePrintStream.printfList;
+//
+//
+//        if(!printList.isEmpty()) {
+//            Assertions.assertTrue(printList.get(0).contains("1000"));
+//        }
+//
+//        if(!printfList.isEmpty()) {
+//            Assertions.assertTrue(printfList.get(0).contains("100.0"));
+//        }
 
-        List<String> printList = StorePrintStream.printlnList;
-        List<String> printfList = StorePrintStream.printfList;
+        provideInput(input);
+        StavNaUcteTester.main(new String[0]);
 
-
-        if(!printList.isEmpty()) {
-            Assertions.assertTrue(printList.get(0).contains("1000"));
-        }
-
-        if(!printfList.isEmpty()) {
-            Assertions.assertTrue(printfList.get(0).contains("100.0"));
-        }
+        Assertions.assertTrue(getOutput().contains("1000") && getOutput().contains("100") && getOutput().contains("500"));
     }
 
 }
