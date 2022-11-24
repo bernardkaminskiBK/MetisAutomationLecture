7e02bbe232a6691a00d4f4fe9ffba67f2f582684
MartinMesko <alegremato@gmail.com>
2022-11-24 16:07:43 +0100

    Refactoring

--- src/main/java/java_spolocna_praca_s_lektorom/Webinar24/Zadanie_24_2_1/Aplikacia.java
+++ src/main/java/java_spolocna_praca_s_lektorom/Webinar24/Zadanie_24_2_1/Aplikacia.java
@@ -3,11 +3,13 @@ package java_spolocna_praca_s_lektorom.Webinar24.Zadanie_24_2_1;
 import javax.swing.*;
 
 public class Aplikacia {
+
+   public static JFrame okno;
     public static void main(String[] args) {
 
         Runnable lambda = () -> {
-            JFrame okno = new JFrame();
 
+            okno = new JFrame();
             okno.setTitle("Grafika 01");
             okno.setSize(1200, 800);
             okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
