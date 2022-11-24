311669ec85a7127bccdaff12a53551f658f1f220
MartinMesko <alegremato@gmail.com>
2022-11-24 12:48:11 +0100

    New test method added.

--- src/test/java/java_spolocna_praca_s_lektorom/webinar15/Zadanie_15_2_1/ZretazenyZoznamTest.java
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar15/Zadanie_15_2_1/ZretazenyZoznamTest.java
@@ -1,12 +1,13 @@
 package java_spolocna_praca_s_lektorom.webinar15.Zadanie_15_2_1;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 public class ZretazenyZoznamTest {
+
+    ZretazenyZoznam studenti = new ZretazenyZoznam();
+
 
     @Test
     void pridajNaZaciatokTest()
@@ -23,7 +24,19 @@ public class ZretazenyZoznamTest {
         ZretazenyZoznam osoby = new ZretazenyZoznam();
         osoby.pridajNaZaciatok("Martin");
         osoby.pridajNaZaciatok("Peter");
+        System.out.println(osoby);
         osoby.zmazZoZaciatku();
         assertNotEquals("Peter", osoby.getPrvy());
     }
+
+    @Test
+    void iteratorPridajTest()
+    {
+        ZoznamIterator iterator = studenti.iteratorZoznam();
+        iterator.pridaj("Milan");
+        iterator.pridaj("Martin");
+        assertThat(studenti.getPrvy()).isEqualTo("Milan");
+    }
+
 }
+
