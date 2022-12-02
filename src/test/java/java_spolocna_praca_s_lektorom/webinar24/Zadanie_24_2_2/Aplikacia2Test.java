cf9bc0845bf03a700721459da7e2798c3caa61ce
MartinMesko <alegremato@gmail.com>
2022-12-02 13:16:26 +0100

    New test method added.

--- src/test/java/java_spolocna_praca_s_lektorom/webinar24/Zadanie_24_2_2/Aplikacia2Test.java
+++ src/test/java/java_spolocna_praca_s_lektorom/webinar24/Zadanie_24_2_2/Aplikacia2Test.java
@@ -1,11 +1,14 @@
 package java_spolocna_praca_s_lektorom.webinar24.Zadanie_24_2_2;
 
+import java_spolocna_praca_s_lektorom.Webinar24.Zadanie_24_2_2.Aplikacia;
 import java_spolocna_praca_s_lektorom.Webinar24.Zadanie_24_2_2.HlavneOkno;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 
 public class Aplikacia2Test {
+    HlavneOkno hlavneOkno = new HlavneOkno();
 
     @Test
     void getNameOfJFrameTitle()
@@ -19,5 +22,20 @@ public class Aplikacia2Test {
         assertEquals("Grafika 02", okno.getTitle());
     }
 
-    
+    @Test
+    void getSizeOfJFrame()
+    {
+        Aplikacia.main(new String[0]);
+        try {
+            Thread.sleep(100);
+            assertThat(hlavneOkno.getSize().width).isEqualTo(1200);
+            assertThat(hlavneOkno.getSize().height).isEqualTo(800);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
 }
