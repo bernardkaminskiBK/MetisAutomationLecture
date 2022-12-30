6f0471ae34cd8aa87b54d93933c7f556cdde0c84
MartinMesko <alegremato@gmail.com>
2022-12-30 10:21:51 +0100

    Refactoring.

---
+++ src/main/java/profun/Lekcia10/part3/PolePorovnavac.java
@@ -0,0 +1,47 @@
+package profun.Lekcia10.part3;
+
+public class PolePorovnavac {
+
+    public static void main(String[] args) {
+        int[] prvePole = {1, 2, 3, 4, 5};
+        int[] druhePole = {1, 2, 3, 4, 5};
+
+        int dlzkaPrvehoPola = prvePole.length;
+        int dlzkaDruhehoPola = druhePole.length;
+
+        if (suPoliaZhodne(prvePole, dlzkaPrvehoPola, druhePole, dlzkaDruhehoPola)) {
+            System.out.println("Polia su rovnake");
+        } else {
+            System.out.println("Polia nie su rovnake");
+        }
+    }
+
+    public static boolean suPoliaZhodne(int[] prvePole, int dlzkaPrvehoPola, int[] druhePole, int dlzkaDruhehoPola) {
+        if (dlzkaDruhehoPola != dlzkaPrvehoPola) {
+            return false;
+        }
+
+        for (int i = 0; i < prvePole.length; i++)
+        {
+            if (prvePole[i] != druhePole[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
+//
+//public class PolePorovnavac {
+//
+//    public static void main(String[] args) {
+//        //TODO: doplnit...
+//    }
+//
+//    public static boolean suPoliaZhodne(int[] prvePole, int dlzkaPrvehoPola, int[] druhePole, int dlzkaDruhehoPola) {
+//        //TODO: doplnit...
+//        return //TODO: doplnit...;
+//    }
+//
+//}
---
+++ src/test/java/profun/Lekcia08/part1/ParkovneTest.java
@@ -0,0 +1,18 @@
+package profun.Lekcia08.part1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
+public class ParkovneTest {
+
+    @ParameterizedTest()
+    @CsvSource(value = {"1.5,2.00", "4.0,2.50", "24.0,10.00", "3.0,2.00", "6.00,3.50"})
+    public void spocitajPoplatkyTest(double hodiny, double ocakavanaHodnota) {
+       double aktualnaHodnota = Parkovne.spocitajPoplatky(hodiny);
+
+       Assertions.assertEquals(ocakavanaHodnota, aktualnaHodnota);
+    }
+
+}
